dc9ec1b4b3730b760846c2af452144ba03b3fffd
Varun932 <varun5nov@gmail.com>
2025-06-19 17:30:32 +0530

    Using an inbuilt module in Node Js

--- Beginners/Hello.js
+++ Beginners/Hello.js
@@ -1,9 +1,11 @@
 const logger = require('./module_export');
-
+var fs = require('fs'); //depicting an inbuilt module
 function HelloWorld(name) {
     console.log('My name is ' + name)
 }
 HelloWorld('Varun');
 
+var filenames = fs.readdirSync('./');
+console.log(filenames);
 //Demonstration of the module export functionality
 logger.module_export('Don !!!');
