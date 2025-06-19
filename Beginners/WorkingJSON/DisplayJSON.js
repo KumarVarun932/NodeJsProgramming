6b903e36881ba37854605a09afeba563b204ac79
Varun932 <varun5nov@gmail.com>
2025-06-19 21:55:27 +0530

    JSON data retrieval

---
+++ Beginners/WorkingJSON/DisplayJSON.js
@@ -0,0 +1,5 @@
+const language = require("./Data")
+
+console.log(language);//It brings the output as an objects with everything
+console.log(language.English); //This allow us to bring the content of the JSON file with Key vaue
+console.log(language.Hindi);
