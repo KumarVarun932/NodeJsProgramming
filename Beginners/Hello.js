ec704e40af71838040e67aa41eab0bfc2d861b4a
Varun932 <varun5nov@gmail.com>
2025-06-19 14:39:21 +0530

    Module export code

--- Beginners/Hello.js
+++ Beginners/Hello.js
@@ -1,4 +1,9 @@
+const logger = require('./module_export');
+
 function HelloWorld(name) {
     console.log('My name is ' + name)
 }
 HelloWorld('Varun');
+
+//Demonstration of the module export functionality
+logger.module_export('Don !!!');
---
+++ Beginners/module_export.js
@@ -0,0 +1,7 @@
+var url = 'https://www.varun.io.com/'
+
+function module_export(name) {
+    console.log(name)
+}
+
+module.exports.module_export = module_export;
