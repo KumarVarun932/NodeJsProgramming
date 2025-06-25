fc8b7a9fc285c4b5ed3fa5ba1c98377dc44892d9
Varun932 <varun5nov@gmail.com>
2025-06-25 12:23:49 +0530

    Getting the JSON data from Package.json

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
+const env = require('dotenv');
+const product_data = require('./Data/products.json');
 
+
+env.config({
+    PATH :'./config/config.env'
+})
 const PORT = process.env.PORT || 3000;
+
 
 app.get('/', (req,res) => {
     res.setHeader('content-type', 'text/html');
     res.send("Hello Express JS");
 })
 
+app.get('/api/v2/products', (req, res) => {
+    res.status(200).send(JSON.stringify(product_data))
+})
 app.listen(3000, () => {
-    console.log("The server is listening on the Port", PORT);
+    const Output = 'The server is listening on the Port, ${PORT}, in the environment  ${ process.env.NODE_ENV }server';
+    console.log(Output);
 })
